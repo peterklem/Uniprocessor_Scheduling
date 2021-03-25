5c78d0fea5a11c8dd85cba0085d6444dd3719133
Peter Klembczyk <klembczykp@wit.edu>
2021-03-25 12:38:31 -0400

    Added to SRT algorithm not complete

--- uniprocessor_scheduling.h
+++ uniprocessor_scheduling.h
@@ -9,11 +9,39 @@
 struct process{
     char name[10];
     int arrival, service, startTime, finishTime, waitTime, turnaroundTime;
+    int executionTime;
+    int arrived, started, finished, running;
 };
 
-void SRT(struct process* processes)
+
+void SRT(struct process** processes)
+// Function: min[service time - time spent in execution] 
+// Checks at arrival to update
 {
-    //    
+    int clk = 0; // Clock
+    int min = 10;
+    int temp; // for comparing to min value
+    int iter; // Keep track of running process
+
+    for(int i = 0; i < NUM_PROCESSES; i++)
+    {
+        if(clk == processes[i]->arrival)
+        {
+            processes[i]->arrived = 1;
+        }
+        if(processes[i]->arrived == 1 && !processes[i]->finished && i > 0)
+        {
+            temp = processes[i]->service - processes[i]->executionTime;
+            if(min > temp)
+            {
+                min = temp;
+                iter = i;
+            }
+        }
+    }
+    //for(int i = 0; i < NUM_PROCESSES; i++)
+    
+
 }
 
 void printResults(struct process** processes)
